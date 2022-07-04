d21670c724f4197b2358de3c1181edb20997658a
unknown <alexzeus@ukr.net>
2022-07-04 11:59:21 +0300

    First commit

---
+++ studybud/urls.py
@@ -0,0 +1,12 @@
+from email.mime import base
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path('api/', include('base.api.urls')),
+    path('api-auth/', include('rest_framework.urls'))
+
+]
